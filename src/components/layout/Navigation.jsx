abc32ce79a352ec7d3abb0efc72ad8397990ab78
Farhan <farhan.midsweden@gmail.com>
2023-02-09 13:54:02 +0200

    Fixed the links

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -57,7 +57,7 @@ const Navigation = () => {
         </li>
         <li className="inline-block align-middle">
           {checkroute ? (
-            <Link href="/homepage3">
+            <Link href="/">
               <a className="group relative inline-block cursor-pointer py-6 text-sm font-medium uppercase tracking-wider text-heading before:text-primary">
                 Resume
                 <span className="absolute left-0 top-auto bottom-5 inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
@@ -80,7 +80,7 @@ const Navigation = () => {
         </li>
         <li className="inline-block align-middle">
           {checkroute ? (
-            <Link href="/homepage3">
+            <Link href="/">
               <a className="group relative inline-block cursor-pointer py-6 text-sm font-medium uppercase tracking-wider text-heading before:text-primary">
                 Works
                 <span className="absolute left-0 top-auto bottom-5 inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
@@ -103,7 +103,7 @@ const Navigation = () => {
         </li>
         <li className="inline-block align-middle">
           {checkroute ? (
-            <Link href="/homepage3">
+            <Link href="/">
               <a className="group relative inline-block cursor-pointer py-6 text-sm font-medium uppercase tracking-wider text-heading before:text-primary">
                 Blog
                 <span className="absolute left-0 top-auto bottom-5 inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
@@ -126,7 +126,7 @@ const Navigation = () => {
         </li>
         <li className="inline-block align-middle">
           {checkroute ? (
-            <Link href="/homepage3">
+            <Link href="/">
               <a className="group relative inline-block cursor-pointer py-6 text-sm font-medium uppercase tracking-wider text-heading before:text-primary">
                 Contact
                 <span className="absolute left-0 top-auto bottom-5 inline-block h-px w-full origin-top-right scale-0 bg-primary align-middle transition-transform duration-500 group-hover:origin-top-left group-hover:scale-100"></span>
